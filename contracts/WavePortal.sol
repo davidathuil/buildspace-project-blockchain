38dc97eb96b97ac0c249eeaa742ffa62df5c6386
David A <david.athuil@gmail.com>
2022-08-30 09:10:55 +0200

    contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+
+    constructor() {
+        console.log("Yo yo i am a contract and i m smart");
+        
+    }
+
+}

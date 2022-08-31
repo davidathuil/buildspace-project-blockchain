67637e33ac4a773b2e0de58d947875511a02fad8
David A <david.athuil@gmail.com>
2022-08-31 18:41:56 +0200

    storedata

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,9 +5,21 @@ import "hardhat/console.sol";
 
 contract WavePortal {
 
+uint256 totalWaves;
+
     constructor() {
         console.log(" i am a contract and i m smart");
         
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+    function wave() public {
+        totalWaves += 1;
+        
+        console.log("%s has waved!", msg.sender);
     }
 
 }
